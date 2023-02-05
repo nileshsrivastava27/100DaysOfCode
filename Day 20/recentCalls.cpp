db62165a1e129544515946f25d4dcafd0a1ba79c
Nilesh Srivastava <nilesh.sri02@gmail.com>
2023-02-05 13:41:05 +0530

    Day 20

---
+++ Day 20/recentCalls.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class RecentCounter {
+public:
+    queue<int> q;
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        q.push(t);
+
+        while(q.front()<t-3000){
+            q.pop();
+
+        }
+        return q.size();
+    }
+};
