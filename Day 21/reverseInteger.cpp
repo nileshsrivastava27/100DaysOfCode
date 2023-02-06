ab37c130887fe59ef2e009360680cce259443ace
Nilesh Srivastava <nilesh.sri02@gmail.com>
2023-02-06 15:27:47 +0530

    Day 21

---
+++ Day 21/reverseInteger.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+
+int reverse(int x) {
+        long r=0;
+
+
+        while(x)
+        {
+            r = r * 10 + x % 10; 
+            x = x / 10;     
+        }
+        if(r > INT_MAX || r < INT_MIN) 
+            return 0; 
+
+        return int(r);
+    }
