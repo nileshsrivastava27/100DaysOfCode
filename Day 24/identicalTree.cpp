27bc6d4c6ed779203b3a202b442b556c96bef1c2
Nilesh Srivastava <nilesh.sri02@gmail.com>
2023-02-09 12:56:16 +0530

    Day 24

---
+++ Day 24/identicalTree.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+
+bool isSameTree(TreeNode* r1, TreeNode* r2) {
+        //base case
+        if(r1==NULL && r2==NULL){
+            return true;
+    }
+    
+        if(r1==NULL && r2!=NULL){
+            return false;
+        }
+        
+        if(r1!=NULL && r2==NULL){
+            return false;
+        }
+    
+        
+        bool left = isSameTree(r1->left, r2->left);
+        bool right = isSameTree(r1->right, r2->right);
+        
+        bool value = r1->val == r2->val;
+        
+        if(left && right&& value){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
