b7d5e4a1fe4da8edf0ff0cd254fc1aa3fd06d7cf
Nilesh Srivastava <nilesh.sri02@gmail.com>
2023-02-20 16:58:38 +0530

    Day 35

---
+++ Day 35/QueueTicket.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+
+
+int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time=0;
+        queue<int> q;
+        int n=tickets.size();
+        for(int i=0;i<n;i++) q.push(i);
+
+        while(true)
+        {
+            if(tickets[k]==0) break;
+            int curr=q.front();
+            time++;
+            tickets[curr]--;
+            q.pop();
+            if(tickets[curr]!=0) 
+            {
+                q.push(curr);
+            }
+        }
+        return time;
+    }
