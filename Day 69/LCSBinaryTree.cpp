56b0183616a5d5badc0fe3403d4473d52f74ad68
Nilesh Srivastava <nilesh.sri02@gmail.com>
2023-04-27 22:32:25 +0530

    Day 69

---
+++ Day 69/LCSBinaryTree.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+
+class TreeNode{
+
+    public:
+
+        int val;
+        TreeNode* left;
+        TreeNode* right;
+
+    TreeNode(int data)
+    {
+        this->val = data;
+        this->right = NULL;
+        this->left = NULL;
+    }
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if(root == NULL)
+        return NULL;
+    
+    if(root->val == p->val || root->val == q->val)
+        return root;
+    
+    TreeNode* leftAns = lowestCommonAncestor(root->left,p,q);
+    TreeNode* rightAns = lowestCommonAncestor(root->right,p,q);
+
+    if(leftAns != NULL && rightAns != NULL)
+        return root;
+    
+    else if(leftAns != NULL && rightAns == NULL)
+        return leftAns;
+    
+    else if(leftAns == NULL && rightAns != NULL)
+        return rightAns;
+    
+    else 
+        return NULL;
+    }
+};
