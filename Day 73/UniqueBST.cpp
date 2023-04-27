0430ac99ebbe31fe9de47bbb1897e25952efd5ae
Nilesh Srivastava <nilesh.sri02@gmail.com>
2023-04-27 22:41:51 +0530

    Day 73

---
+++ Day 73/UniqueBST.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    // int solveMem(int n, vector<int> &dp)
+    // {
+
+    //     if(n <= 1)
+    //         return 1;
+
+    //     if(dp[n] != -1)
+    //         return dp[n];
+
+    //     int ans = 0;
+
+    //     for(int i=1;i<=n;i++){
+
+    //         ans+= solve(i - 1, dp) * solve(n - i, dp);
+    //     }
+
+    //     return dp[n] = ans;
+    // }
+
+
+    int solveTab(int N)
+    {
+
+        vector<int> dp(N+1, 0);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        int ans = 0;
+
+        for(int n = 2; n<=N;n++)
+        {
+            int ans = 0;
+
+            for(int i=1;i<=n;i++){
+
+                ans+= dp[i - 1] * dp[n - i];
+            }
+
+            dp[n] = ans;
+        }
+
+        return dp[N];
+
+    }
+
+
+
+    int numTrees(int n) {
+        vector<int> dp(n+1, -1);
+
+        return solveTab(n);
+    }
+};
