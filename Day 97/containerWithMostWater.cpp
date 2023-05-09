70a380c9cf0825253ab0b8621561e9c08082bca4
Nilesh Srivastava <nilesh.sri02@gmail.com>
2023-05-09 17:25:22 +0530

    Day 97

---
+++ Day 97/containerWithMostWater.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        
+        int res = 0;
+        int left = 0;
+        int right = height.size()-1;
+
+        while(left<=right)
+        {
+        int temp = (right-left) * min(height[left], height[right]);
+
+        res = max(res, temp);
+
+        if(height[left] <= height[right])
+            left++;
+        else
+            right--;
+        }
+        return res;
+    }
+};
