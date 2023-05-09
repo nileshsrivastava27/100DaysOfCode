a6f029338960433efca64c7f08fc12f800aad138
Nilesh Srivastava <nilesh.sri02@gmail.com>
2023-05-09 17:09:58 +0530

    Day 87

---
+++ Day 87/SortPeople.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        
+    vector<pair<int, string>> v;
+    int n=heights.size();
+    
+    for (int i = 0; i < n; i++)
+        v.push_back({heights[i], names[i]});
+
+    sort(v.begin(),v.end());
+    vector<string> ans;
+
+    for (int i = n-1; i >=0; i--)
+        ans.push_back(v[i].second);
+    return ans;
+    }
+};
